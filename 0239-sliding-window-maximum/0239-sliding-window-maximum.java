c5c8853d0243077978e58ee0a02d22cda325a8da
INDHUREDDY09 <vtu29455@veltech.edu.in>
2026-08-29 14:37:24 +0530

    Time: 29 ms (85.11%), Space: 143.4 MB (87.81%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,26 +1,36 @@
-import java.util.*;
-
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+
         int n = nums.length;
         int[] result = new int[n - k + 1];
+
         Deque<Integer> deque = new ArrayDeque<>();
-        int index = 0;
+
+        int resultIndex = 0;
 
         for (int i = 0; i < n; i++) {
-            while (!deque.isEmpty() && deque.peekFirst() <= i - k)
+
+            // Remove elements that are outside the window
+            if (!deque.isEmpty() && deque.peekFirst() <= i - k) {
                 deque.pollFirst();
+            }
 
-            while (!deque.isEmpty() &&
-                   nums[deque.peekLast()] <= nums[i])
+            // Remove smaller elements
+            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
                 deque.pollLast();
+            }
 
+            // Add current index
             deque.offerLast(i);
 
-            if (i >= k - 1)
-                result[index++] = nums[deque.peekFirst()];
+            // Store maximum
+            if (i >= k - 1) {
+                result[resultIndex] = nums[deque.peekFirst()];
+                resultIndex++;
+            }
         }
 
         return result;
     }
 }
+
