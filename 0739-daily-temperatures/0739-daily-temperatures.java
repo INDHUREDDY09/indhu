79a5bc3e847be78cfde7971e0170e70c6ceb0b91
INDHUREDDY09 <vtu29455@veltech.edu.in>
2026-08-29 14:07:19 +0530

    Time: 71 ms (38.74%), Space: 107.7 MB (23.82%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,20 +1,15 @@
-import java.util.*;
-
 class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        int n = temperatures.length;
-        int[] answer = new int[n];
+    public int[] dailyTemperatures(int[] temps) {
+        int[] results = new int[temps.length];
         Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() &&
-                   temperatures[i] > temperatures[stack.peek()]) {
-                int index = stack.pop();
-                answer[index] = i - index;
+        /// UPVOTE !
+        for (int i = 0; i < temps.length; i++) {
+            while (!stack.isEmpty() && temps[stack.peek()] < temps[i]) {
+                results[stack.peek()] = i - stack.pop();
             }
             stack.push(i);
         }
 
-        return answer;
+        return results;
     }
 }
