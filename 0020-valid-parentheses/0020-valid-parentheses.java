f4fd6b3a66e27b8ff25d710133da68ec78246c8b
INDHUREDDY09 <vtu29455@veltech.edu.in>
2026-09-09 09:18:43 +0530

    Time: 3 ms (86.08%), Space: 43.1 MB (85.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,30 @@
-import java.util.*;
-
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
+
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
-            } else {
-                if (stack.isEmpty())
+            } 
+            else {
+                if (stack.isEmpty()) {
                     return false;
+                }
 
                 char top = stack.pop();
 
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '['))
+                if (c == ')' && top != '(') {
                     return false;
+                }
+
+                if (c == '}' && top != '{') {
+                    return false;
+                }
+
+                if (c == ']' && top != '[') {
+                    return false;
+                }
             }
         }
 
