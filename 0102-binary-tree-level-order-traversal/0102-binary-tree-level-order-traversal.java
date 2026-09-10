41915d5ab0bd00ebb64c55b0ffa68962eb626c48
indhu <vtu29455@veltech.edu.in>
2026-09-10 15:27:42 +0530

    Time: 1 ms (95.8%), Space: 47 MB (7.75%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,26 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
+         List<List<Integer>> ans = new ArrayList<>();
 
-        if(root==null) return result;
-              Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null){
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
 
-          while (!queue.isEmpty()) {
-            int size = queue.size();
+        while(!queue.isEmpty()) {
+
+            int size = queue.size(); 
             List<Integer> level = new ArrayList<>();
 
-            for(int i=0;i<size;i++){
-                TreeNode node=queue.poll();
-                level.add(node.val);
+            for(int i = 0; i < size; i++){
 
-                if(node.left!=null)queue.offer(node.left);
-                if(node.right!=null)queue.offer(node.right);
+                TreeNode curr = queue.poll();
+                level.add(curr.val);
 
+                if(curr.left != null){
+                    queue.offer(curr.left);
+                }
+
+                if(curr.right != null){
+                    queue.offer(curr.right);
+                }
             }
-            result.add(level);
+
+            ans.add(level);
         }
-        return result;
+
+        return ans;
     }
 }
-
