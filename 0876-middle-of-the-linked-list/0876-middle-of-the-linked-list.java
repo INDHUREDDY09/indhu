1a76615d00e94abeba8c5fb0f57b0c501a99fa48
indhu <vtu29455@veltech.edu.in>
2026-09-10 14:40:13 +0530

    Time: 0 ms (100%), Space: 42.6 MB (93.31%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -5,23 +5,17 @@
  *     ListNode next;
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) {
- *         this.val = val;
- *         this.next = next;
- *     }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
+        if(head==null || head.next == null) return head;
+        ListNode slow=head,fast=head;
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
-
         return slow;
     }
 }
