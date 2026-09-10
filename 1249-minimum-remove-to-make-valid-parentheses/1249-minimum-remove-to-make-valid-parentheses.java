fd4c5503dbaff087b2431852c194c563bd10036e
indhu <vtu29455@veltech.edu.in>
2026-09-10 15:05:14 +0530

    Time: 23 ms (29.75%), Space: 47.8 MB (13.43%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,33 +1,16 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
-
-        // Remove invalid ')'
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                count++;
-                sb.append(c);
-            } 
-            else if (c == ')') {
-                if (count > 0) {
-                    count--;
-                    sb.append(c);
-                }
-            } 
-            else {
-                sb.append(c);
-            }
-        }
-
-        // Remove extra '(' from right to left
-        for (int i = sb.length() - 1; i >= 0 && count > 0; i--) {
-            if (sb.charAt(i) == '(') {
-                sb.deleteCharAt(i);
-                count--;
-            }
-        }
-
-        return sb.toString();
+       StringBuilder sb = new StringBuilder(s);
+  Stack<Integer> st = new Stack<>();
+  for (int i = 0; i < sb.length(); ++i) {
+    if (sb.charAt(i) == '(') st.add(i);
+    if (sb.charAt(i) == ')') {
+      if (!st.empty()) st.pop();
+      else sb.setCharAt(i, '*');
+    }
+  }
+  while (!st.empty())
+    sb.setCharAt(st.pop(), '*');
+  return sb.toString().replaceAll("\\*", ""); 
     }
 }
