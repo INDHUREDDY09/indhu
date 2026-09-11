22248de5fd472396c16c9fd277520b23abb8e4a7
indhu <vtu29455@veltech.edu.in>
2026-09-11 14:51:56 +0530

    Time: 1 ms (95.79%), Space: 46.7 MB (61.3%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,51 +1,37 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-         List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
-        if(root == null){
-            return ans;
+        if (root == null) {
+            return result;
         }
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
 
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
 
-            int size = queue.size(); 
-            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
 
-            for(int i = 0; i < size; i++){
+                currentLevel.add(node.val);
 
-                TreeNode curr = queue.poll();
-                level.add(curr.val);
-
-                if(curr.left != null){
-                    queue.offer(curr.left);
+                if (node.left != null) {
+                    queue.offer(node.left);
                 }
 
-                if(curr.right != null){
-                    queue.offer(curr.right);
+                if (node.right != null) {
+                    queue.offer(node.right);
                 }
             }
 
-            ans.add(level);
+            result.add(currentLevel);
         }
 
-        return ans;
+        return result;
     }
 }
